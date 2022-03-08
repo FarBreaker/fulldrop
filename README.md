bee6e09a204f97d9accc7169ec89d11c6a92aaba
Lorenzo Ciani <cianilorenzo3@gmail.com>
2022-03-08 12:13:45 +0100

    Repo init

---
+++ README.md
@@ -0,0 +1,5 @@
+<!-- @format -->
+
+#Hello
+
+##Syntax have been changed
---
+++ bin/index.js
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+const shell = require('shelljs')
+const yargs = require('yargs')
+const chalk = require('chalk')
+const chalkAnimation = require('chalk-animation')
+
+console.log(chalk.cyan('Executing release procedure please wait'))
+const argv = yargs
+    .command('release', 'Command to start release sequence', {
+        versionToRelease:
+        {
+            description: 'The version to release',
+            alias: 'v',
+            type: "string",
+            default: 'prerelease',
+            choices: ['major', 'minor', 'patch', 'premajor', 'preminor', 'prepatch', 'prerelease']
+        },
+        commitMessage:
+        {
+            description: "Message to commit",
+            alias: "c",
+            type: "string",
+            default: undefined
+        }
+    })
+    .help().alias('help', "h").argv
+
+if (argv._.includes('release')) {
+    const anim = chalkAnimation.karaoke(`release mode is set to ` + chalk.cyan(argv.versionToRelease))
+    shell.exec(` npm version ${argv.versionToRelease} --no-git-tag`)
+    shell.exec(` git add .`)
+    shell.exec(` git commit -m "${argv.commitMessage}"`)
+    shell.exec(` git pull && git push`)
+    setTimeout(() => {
+        anim.stop()
+    }, 2500)
+}
